5681938875829b0cf06da73861dadcadf78aee26
Lohita Venkatesh <lohitavenkatesh@gmail.com>
2025-02-24 11:25:56 +0530

    Complete frontend

---
+++ admin/scripts.js
@@ -0,0 +1,9 @@
+function toggleNavbar() {
+    const navbarCollapse = document.getElementById('navbarCollapse');
+    if (navbarCollapse.style.display === 'block') {
+      navbarCollapse.style.display = 'none';
+    } else {
+      navbarCollapse.style.display = 'block';
+    }
+  }
+  
